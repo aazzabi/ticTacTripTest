e3cf525899b1872f7713d794638e0026cad5d798
Arafet <arafet.azzabi@esprit.tn>
2020-09-18 15:00:02 +0100

    Create Model Request

---
+++ models/Request.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const RequestSchema = new mongoose.Schema({
+    name: {type: String, required: true},
+    email: {type: String, required: true, unique: true},
+    count: {type: Number, default : 0, required: false},
+    date: {type: Date, default: Date.now}
+
+});
+
+module.exports = Request = mongoose.model('request', RequestSchema);
