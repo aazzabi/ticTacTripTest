a6f6183bae31ca8fa98dc7f1687fd1d64e4d98c3
Arafet <arafet.azzabi@esprit.tn>
2020-09-19 11:18:58 +0100

    Testing if token allow text justifying

--- controllers/TicTacController.js
+++ controllers/TicTacController.js
@@ -3,7 +3,6 @@ var mongoose = require('mongoose');
 const {check, validationResult} = require("express-validator/check");
 var jwt = require('jsonwebtoken');
 const config = require("../config/config");
-const countWords = require("count-words");
 
 var getAll = (req, res, next) => {
     Req.find({}).then((data) => {
@@ -22,40 +21,86 @@ var getByEmail = (req, res, next) => {
     });
 };
 
-var justify = (req, res, next) => {
+var justify = async (req, res, next) => {
     res.type("text/plain");
     var text = req.body;
+    const token = req.header("token");
+    const decoded = jwt.verify(token, config.authentification.secret);
+    const totalWords = text.trim().split(/\s+/).length;
+
+    var cmp = 80;
+    var result = "";
+    var j;
+    var wordsRemainingAllowed = 0;
 
     // Check content
     if (!text) {
         res.send('');
         return;
     }
-    var cmp = 80;
-    var result = "";
-    var j;
-    // console.log(text.trim().split(/\s+/).length , 'text');
     text = text.replace(/\s\s+/g, ' ');
 
-    for (var i = 0; i < text.length; i++) {
-        result += text[i];
-        if (i == cmp) {
-            if (text[i] == ' ' || text[i] == ',' || text[i] == '.') {
-                result += '\n';
-                cmp = i + 1 + 80;
-            } else {
-                j = 0;
-                while (text[i] !== ' ' && text[i] !== '.' && text[i] !== ',') {
-                    i = i - 1;
-                    j++;
+    //calculate words allowed to be justified
+    await Req.findOne({email: decoded.email}).then((data) => {
+        wordsRemainingAllowed = 80000 - data.count;
+    });
+
+    // if wordsAllowedRemaining >= totalWords in the body  ==> okey , you can justify text
+    if (wordsRemainingAllowed >= totalWords) {
+        for (var i = 0; i < text.length; i++) {
+            result += text[i];
+            if (i == cmp) {
+                if (text[i] == ' ' || text[i] == ',' || text[i] == '.') {
+                    result += '\n';
+                    cmp = i + 1 + 80;
+                } else {
+                    j = 0;
+                    while (text[i] !== ' ' && text[i] !== '.' && text[i] !== ',') {
+                        i = i - 1;
+                        j++;
+                    }
+                    result = result.substr(0, result.length - j);
+                    result += '\n';
+                    cmp = i + 80;
                 }
-                result = result.substr(0, result.length - j);
-                result += '\n';
-                cmp = i + 80;
             }
         }
+        res.send(addSpace(result).join('\n'));
+    } else {
+        res.status(402).send("402 Payment Required");
+
+        /*
+        **********************************************************************************
+        **    An other solution :
+        **    we can allow justifying text only for the remaining allowed number of words,
+        **    and sending back the allowed result
+        **********************************************************************************
+
+        let lastAllowed = (totalWords - wordsRemainingAllowed) - 1;
+        text = text.trim().split(/\s+/).splice(0, totalWords - lastAllowed).join(' ');
+        for (var i = 0; i < text.length; i++) {
+            result += text[i];
+            if (i == cmp) {
+                if (text[i] == ' ' || text[i] == ',' || text[i] == '.') {
+                    result += '\n';
+                    cmp = i + 1 + 80;
+                } else {
+                    j = 0;
+                    while (text[i] !== ' ' && text[i] !== '.' && text[i] !== ',') {
+                        i = i - 1;
+                        j++;
+                    }
+                    result = result.substr(0, result.length - j);
+                    result += '\n';
+                    cmp = i + 80;
+                }
+            }
+        }
+        res.send(addSpace(result).join('\n'));
+
+
+         */
     }
-    res.send(addSpace(result).join('\n'))
 };
 
 function addSpace(text) {
@@ -90,19 +135,17 @@ function addCharAtIndex(str, index, chr) {
 
 var generateToken = async (req, res, next) => {
     //Check errors in  the body
-
     const errors = validationResult(req);
 
-    //Bad Request
     if (!errors.isEmpty()) {
         return res.status(400).json({
             errors: errors.array()
         });
     }
-    const {email} = req.body.email;
+    const email = req.body.email;
     //ParserBody
     try {
-        // See if user exists
+        // See if request exists
         let rq = await Req.findOne({
             email
         });
@@ -114,19 +157,36 @@ var generateToken = async (req, res, next) => {
                 {
                     expiresIn: 360000
                 },
-                (err, token) => {
+                async (err, token) => {
                     if (err) throw err;
-                    res.json({
-                        token,
-                    });
+                    await Req.create({email: email, token: token});
+                    await res.json({token,});
                 }
             );
+        } else if (diffDate(rq.date, new Date()) > 1) {  // delay > 24h : update (COUNT = 0) && (DATE= now)
+            let updatedReq = {
+                email: rq.email,
+                token: rq.token,
+                date: new Date(),
+                count: 0
+            };
+            await Req.update({email: email}, updatedReq);
+            res.json({token: rq.token,})
+        } else {
+            res.json({token: rq.token,}); // else => a request exists with the same email
         }
     } catch (error) {
         console.log(error.message);
         res.status(500).send("server Error");
     }
 };
+
+function diffDate(d1, d2) {
+    dt1 = new Date(d1);
+    dt2 = new Date(d2);
+    return Math.floor((Date.UTC(dt2.getFullYear(), dt2.getMonth(), dt2.getDate()) - Date.UTC(dt1.getFullYear(), dt1.getMonth(), dt1.getDate())) / (1000 * 60 * 60 * 24));
+}
+
 module.exports = {
     getAll,
     getByEmail,
--- models/Request.js
+++ models/Request.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const RequestSchema = new mongoose.Schema({
-    name: {type: String, required: true},
+    token: {type: String, required: true},
     email: {type: String, required: true, unique: true},
     count: {type: Number, default : 0, required: false},
     date: {type: Date, default: Date.now}
