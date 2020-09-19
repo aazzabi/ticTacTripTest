c2675800f179748d2899a254e3fbfe98d1a3beb5
Arafet <arafet.azzabi@esprit.tn>
2020-09-19 11:37:11 +0100

    delete plainTextParser

--- routes/tictac.js
+++ routes/tictac.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 const TicTacController = require("../controllers/TicTacController");
 const {check, validationResult} = require("express-validator/check");
-const plainTextParser = require('plainTextParser');
 const TokenExist = require("../middlewares/TokenExist");
 
 router.get('/', TicTacController.getAll);
